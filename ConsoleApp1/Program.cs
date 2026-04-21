0d21931203af5e2a0b2db81c0a1630a8264a0864
Stasyamr4 <anastasiamura04@mail.ru>
2026-04-21 15:52:35 +0300

    выполнена отладка и добавлены xml комментарии

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,15 +5,23 @@ namespace Galaktikos
 {
     class Program
     {
+        /// <summary>
+        /// Главная точка входа. Запускает приветствие и вывод списка галактик.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки (не используются)</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Galaxy News!");
-            IterateThroughList();
-            Console.ReadKey();
+            IterateThroughList(); // Выводим информацию о галактиках
+            Console.ReadKey();    // Ждём нажатия клавиши перед закрытием
         }
 
+        /// <summary>
+        /// Создаёт коллекцию галактик и выводит их свойства в консоль.
+        /// </summary>
         private static void IterateThroughList()
         {
+            // Инициализируем список галактик с помощью коллектора объектов
             var theGalaxies = new List<Galaxy>
         {
             new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
@@ -23,10 +31,10 @@ namespace Galaktikos
             new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
             new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
         };
-
+            // Перебираем каждую галактику и форматируем вывод
             foreach (Galaxy theGalaxy in theGalaxies)
             {
-                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType);
+                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType.MyGType);
             }
 
             // Expected Output:
@@ -38,20 +46,32 @@ namespace Galaktikos
             //  Maffei 1,  11,  Elliptical
         }
     }
-
+    /// <summary>
+    /// Представляет галактику с именем, расстоянием и морфологическим типом.
+    /// </summary>
     public class Galaxy
     {
+        /// <summary>Название галактики</summary>
         public string Name { get; set; }
-
+        /// <summary>Расстояние до галактики в миллионах световых лет</summary>
         public double MegaLightYears { get; set; }
-        public object GalaxyType { get; set; }
+        /// <summary>Морфологический тип галактики (спиральная, эллиптическая и т.д.)</summary>
+        public GType GalaxyType { get; set; }
 
     }
-
+    /// <summary>
+    /// Класс-обёртка для преобразования символьного кода типа галактики ('S', 'E'...) 
+    /// в человекочитаемое значение.
+    /// </summary>
     public class GType
     {
+        /// <summary>
+        /// Инициализирует тип галактики на основе однобуквенного кода.
+        /// </summary>
+        /// <param name="type">Код типа: 'S'=Spiral, 'E'=Elliptical, 'I'=Irregular, 'L'=Lenticular</param>
         public GType(char type)
         {
+            // Сопоставляем буквенный код с перечислением
             switch (type)
             {
                 case 'S':
@@ -60,7 +80,7 @@ namespace Galaktikos
                 case 'E':
                     MyGType = Type.Elliptical;
                     break;
-                case 'l':
+                case 'I':
                     MyGType = Type.Irregular;
                     break;
                 case 'L':
@@ -70,7 +90,13 @@ namespace Galaktikos
                     break;
             }
         }
+        /// <summary>Человекочитаемое название типа галактики</summary>
+        /// <remarks>Возвращает значение перечисления Type как object</remarks>
         public object MyGType { get; set; }
+        /// <summary>
+        /// Внутреннее перечисление типов галактик.
+        /// Скрыто от внешнего использования (private).
+        /// </summary>
         private enum Type { Spiral, Elliptical, Irregular, Lenticular }
     }
 }
