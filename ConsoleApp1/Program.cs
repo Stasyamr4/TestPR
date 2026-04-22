2036e74d2fe6b6e77ab70ce2276987dd7c177534
Stasyamr4 <anastasiamura04@gmail.com>
2026-04-22 16:18:49 +0300

    начало работы над проектом "Буквы"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,26 +1,23 @@
-﻿int result = Fibonacci(5);
-Console.WriteLine(result);
+﻿using System;
 
-
-/// <summary>
-/// Вычисляет n-е число Фибоначчи итеративным способом.
-/// </summary>
-/// <param name="n">Порядковый номер числа (нумерация с 0: F(0)=0, F(1)=1)</param>
-/// <returns>n-е число Фибоначчи</returns>
-static int Fibonacci(int n)
+class ArrayExample
 {
-    Console.WriteLine("The output is: ");
-    int n1 = 0;
-    int n2 = 1;
-    int sum;
-
-    // Цикл начинается с 2, так как первые два числа уже заданы
-    for (int i = 2; i <= n; i++)
+    static void Main()
     {
-        sum = n1 + n2; // Следующее число = сумма двух предыдущих
-        n1 = n2;
-        n2 = sum;
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
     }
-    // При n=0 возвращаем n1, иначе n2 (последнее вычисленное значение)
-    return n == 0 ? n1 : n2;
+
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
 }
